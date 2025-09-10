c43070d28e107d1e188e52ac185336a40da14b4f
nasrunal <e42241414@student.polije.ac.id>
2025-09-10 15:12:14 +0700

    kamuadalahjava

---
+++ javascript.js
@@ -0,0 +1,20 @@
+alert("Welcome Home!");
+confirm("click OK if you want a next this page!");
+if (confirm) {
+  let Date = prompt("Whats Your Birthday?");
+  alert("Ouh, My Birthday" + Date + "Year");
+} else {
+  alert("Its Okay, Maybe Next Leter");
+}
+
+prompt("Please, Put Your Name", "Nasrun Al amin");
+function foo() {
+  alert("Okay Welcome Guys!!!!");
+}
+foo();
+a;
+Date("2025-01-31");
+parselnt("1");
+setTimeout(function () {
+  alert("This is Alert from setTimeout");
+}, 3000);
